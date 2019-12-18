52b1c71a3991b35b068201f2ee7781abe4730d48
weng lin <wenglin@buffalo.edu>
2019-12-18 01:24:45 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -67,10 +67,7 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{   :yukihiro_matsumoto => {
-		      :known_for => "Ruby",
-		      :languages => ["LISP", "C"]
-		   },
+ 		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,7 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto] = {
+ 		      :known_for => "Ruby",
+ 		      :languages => ["LISP", "C"]}
+		programmer_hash
 end
 
 def changing_alan
