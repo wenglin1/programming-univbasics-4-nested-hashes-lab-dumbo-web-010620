8256f42fe577774a0dace3e1e838a7c1b3947a0d
weng lin <wenglin@buffalo.edu>
2019-12-18 01:22:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -125,6 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+		 programmer_hash[:dennis_ritchie][:languages][1] = ["Assembly"]
 		 programmer_hash
 end
